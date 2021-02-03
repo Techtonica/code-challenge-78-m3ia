92e2a306dcc683c742c3e0eeaca6afda4cf9fb47
Meia Natividad <meianatividad@gmail.com>
2021-02-03 10:56:43 -0800

    add initial solution

--- challenges/78.js
+++ challenges/78.js
@@ -6,8 +6,22 @@
 //
 // has22([2, 1, 2]) → false
 
-function has22(come up with a good, self-explanatory arg name){
-    // your code here
-}
+function has22(arr){
+    
+    //loop through each int.
+    for ( let i=0; i<arr.length; i++ ) {
+        
+        // check if arr[i] is 2 and if arr[i+1] is 2 
+        if ( arr[i] === 2 && arr[i+1] === 2) {    
+
+            return true;
+            }
+
+        //return t/f by assuming false until true
+        } return false;
+} 
+
+// trial line:
+// console.log(has22([1, 2, 1, 2]));
 
 module.exports = has22;
